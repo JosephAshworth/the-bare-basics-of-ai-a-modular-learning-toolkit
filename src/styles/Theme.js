2cf7711fef981732c5f8907b61352161f5ad08a5
Joseph Ashworth <157481258+JosephAshworth@users.noreply.github.com>
2025-05-14 22:13:39 +0100

    Update Theme.js

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -19,32 +19,32 @@ const baseTheme = {
     // font sizes and weights for the different heading levels
     h1: {
       fontSize: '2.5rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     },
     h2: {
       fontSize: '2rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     },
     h3: {
       fontSize: '1.75rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     },
     h4: {
       fontSize: '1.5rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     },
     h5: {
       fontSize: '1.25rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     },
     h6: {
       fontSize: '1rem', 
-      fontWeight: 600, 
+      fontWeight: 600
     }
   },
   // default border radius for the compnents, unless overridden
   shape: {
-    borderRadius: 8, 
+    borderRadius: 8
   },
 
   // by default, apply no text transformation to the text of the buttons
@@ -54,7 +54,7 @@ const baseTheme = {
       styleOverrides: {
         root: {
           textTransform: 'none', 
-          fontWeight: 500, 
+          fontWeight: 500
         }
       }
     }
@@ -72,20 +72,20 @@ export const lightTheme = createTheme({
     primary: {
       main: '#1976d2', // dark blue
       light: '#42a5f5', // light blue
-      dark: '#1565c0', // darker blue
+      dark: '#1565c0' // darker blue
     },
     secondary: {
       main: '#9c27b0', // purple
       light: '#ba68c8', // lighter purple
-      dark: '#7b1fa2', // darker purple
+      dark: '#7b1fa2' // darker purple
     },
     background: {
       default: '#f5f5f5', // white-grey
-      paper: '#ffffff', // white
+      paper: '#ffffff' // white
     },
     text: {
       primary: '#212121', // dark grey
-      secondary: '#757575', // grey
+      secondary: '#757575' // grey
     }
   }
 });
@@ -100,20 +100,20 @@ export const darkTheme = createTheme({
     primary: {
       main: '#90caf9', // light blue
       light: '#e3f2fd', // lighter blue
-      dark: '#42a5f5', // darker blue
+      dark: '#42a5f5' // darker blue
     },
     secondary: {
       main: '#ce93d8', // pink
       light: '#f3e5f5', // lighter pink
-      dark: '#ab47bc', // purple
+      dark: '#ab47bc' // purple
     },
     background: {
       default: '#121212', // black
-      paper: '#1e1e1e', // dark grey
+      paper: '#1e1e1e' // dark grey
     },
     text: {
       primary: '#ffffff', // white
-      secondary: '#b0b0b0', // light grey
+      secondary: '#b0b0b0' // light grey
     }
   }
 });
